b16a7699c6e59cba68eeb3d14292c0e658402e07
frissyn <62220201+frissyn@users.noreply.github.com>
2021-06-12 15:43:38 +0000

    🏷️: release for v0.0.2 (tag)

--- pyke/__init__.py
+++ pyke/__init__.py
@@ -7,7 +7,7 @@ import typing as t
 
 __title__       = "pyke"
 __author__      = "frissyn"
-__version__     = "0.0.1"
+__version__     = "0.0.2"
 __license__     = "MIT"
 __package__     = "pyke"
 __description__ = "Make-like build tool for Python."
